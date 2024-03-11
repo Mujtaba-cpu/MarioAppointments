2c7508e4f03b3dae2b15470367b837c6495b4194
mujtaba-cpu <@mujtabaahmed5358@gmail.com>
2024-03-11 15:09:05 +0500

    deployment

--- frontend/src/pages/search.js
+++ frontend/src/pages/search.js
@@ -4,47 +4,13 @@ import AppointmentDetails from '../components/AppointmentDetails';
 
 const Search = () => {
     const [appointments, setAppointments] = useState([]);
-    const [startDate, setStartDate] = useState('');
-    const [endDate, setEndDate] = useState('');
+    const [start, setStart] = useState('');
+    const [end, setEnd] = useState('');
     const [range, setRange] = useState('today');
 
-    const fetchAppointmentsByDate = async () => {
 
-        const response = await fetch('/appointments/date', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ startDate, endDate }),
-        });
-        const json = await response.json();
-        if (response.ok) {
-            if (json.appointments && Array.isArray(json.appointments)) {
-                setAppointments(json.appointments);
-            } else {
-                console.error('Data from server is not in the expected format' + JSON.stringify(json));
-            }
-        }
-    };
-
-
-    const fetchAppointments = async () => {
-        setStartDate('');
-        setEndDate('');
-        const response = await fetch('/appointments');
-        const json = await response.json();
-        if (response.ok) {
-            if (json.appointments && Array.isArray(json.appointments)) {
-                setAppointments(json.appointments);
-            } else {
-                console.error('Data from server is not in the expected format' + JSON.stringify(json));
-            }
-        }
-
-    };
-    
     const updateAppointments = () => {
-        fetchAppointments();
+        fetchAppointmentsByDateRange();
     };
     const fetchAppointmentsByDateRange = async (range) => {
         const currentDate = new Date();
@@ -54,14 +20,11 @@ const Search = () => {
             startDate = currentDate.toISOString().split('T')[0];
             endDate = startDate;
             console.log(startDate, endDate);
-            fetchAppointmentsByDate();
         } else if (range === 'thisWeek') {
-                    const firstDayOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay()));
-                    const lastDayOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 6));
-                    startDate = firstDayOfWeek.toISOString().split('T')[0];
-                    endDate = lastDayOfWeek.toISOString().split('T')[0];
-                    fetchAppointmentsByDate();
-
+            const firstDayOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay()));
+            const lastDayOfWeek = new Date(currentDate.setDate(currentDate.getDate() - currentDate.getDay() + 6));
+            startDate = firstDayOfWeek.toISOString().split('T')[0];
+            endDate = lastDayOfWeek.toISOString().split('T')[0];
             console.log(startDate, endDate);
         } else if (range === 'thisMonth') {
             const firstDayOfMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 1);
@@ -70,37 +33,67 @@ const Search = () => {
             endDate = lastDayOfMonth.toISOString().split('T')[0];
             console.log(startDate, endDate);
         } else if (range === 'customRange') {
-            startDate = startDate;
-            endDate = endDate;
+            startDate = start;
+            endDate = end;
             console.log(startDate, endDate);
+        }
+
+        const response = await fetch('/appointments/date', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ startDate, endDate }),
+        });
+        console.log(response);
+        const json = await response.json();
+        console.log(json.appointments);
+        if (response.ok) {
+            if (json.appointments && Array.isArray(json.appointments)) {
+                setAppointments(json.appointments);
+            } else {
+                console.error('Data from server is not in the expected format' + JSON.stringify(json));
+            }
         }
     };
     useEffect(() => {
         fetchAppointmentsByDateRange(range);
     }, []);
 
+    const handleFetchButtonClick = () => {
+        if (range !== 'customRange') {
+            fetchAppointmentsByDateRange(range);
+        } else {
+            if (start && end) {
+                fetchAppointmentsByDateRange(range);
+            } else {
+                console.error('Please select both start and end dates for custom range.');
+            }
+        }
+    }
+
 
 
     return (
         <div className='search-box'>
             <div className='search-form-container'>
-            <select value={range} onChange={(e) => setRange(e.target.value)}>
-                <option value="today">Today</option>
-                <option value="thisWeek">This Week</option>
-                <option value="thisMonth">This Month</option>
-                <option value="customRange">Custom Range</option>
-            </select>
-            
-            {range === 'customRange' && (
-                <>
-                    <input type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
-                    <label>End Date:</label>
-                    <input type="date" value={endDate} onChange={(e) => setEndDate(e.target.value)} />
-                </>
-            )}
+                <select value={range} onChange={(e) => setRange(e.target.value)}>
+                    <option value="today">Today</option>
+                    <option value="thisWeek">This Week</option>
+                    <option value="thisMonth">This Month</option>
+                    <option value="customRange">Custom Range</option>
+                </select>
+
+                {range === 'customRange' && (
+                    <>
+                        <input type="date" value={start} onChange={(e) => setStart(e.target.value)} />
+                        <label>End Date:</label>
+                        <input type="date" value={end} onChange={(e) => setEnd(e.target.value)} />
+                    </>
+                )}
             </div>
-            
-            <button style={{ marginRight: '10px'}} onClick={() => fetchAppointmentsByDateRange(range)} >Fetch Appointments</button>
+
+             <button style={{ marginRight: '10px' }} onClick={handleFetchButtonClick}>Fetch Appointments</button>
             {/* <button onClick={fetchAppointments}>View All</button> */}
 
             <div className='workouts'>
