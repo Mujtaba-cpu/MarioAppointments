de2a8da800ae7d834952efaf46c914f386ca4613
mujtaba-cpu <@mujtabaahmed5358@gmail.com>
2024-03-13 16:12:23 +0500

    tweeeeeks

--- frontend/src/components/AppointmentDetails.js
+++ frontend/src/components/AppointmentDetails.js
@@ -62,6 +62,7 @@ const AppointmentDetails = ({ appointment, updateAppointments }) => {
     };
 
     const handleCancel = () => {
+        setModalIsOpen(false)
         setEditMode(false);
         setid('');
         setNumberOfPassengers('');
@@ -82,6 +83,7 @@ const AppointmentDetails = ({ appointment, updateAppointments }) => {
     };
 
     const handleSubmit = async (e) => {
+        setModalIsOpen(false)
         e.preventDefault();
         const response = await fetch(`https://mario-appointments-server.vercel.app/appointments/${appointment._id}`, {
             method: 'PATCH',
@@ -142,198 +144,209 @@ const AppointmentDetails = ({ appointment, updateAppointments }) => {
     return (
         <div className="appointment-details">
             <div className="appointment-grid">
-            <h4>{new Date(appointment.pickupDate).toLocaleDateString('en-GB')}</h4>
-            <p>
-                <strong>PickupLocation:</strong> {appointment.pickupLocation}
-            </p>
-            <p>
-                <strong>Destination:</strong> {appointment.destination}
-            </p>
-            <div className="button-group">
-                <span className='material-symbols-outlined' onClick={handleClick}>🗑️</span>
-                <span className='material-symbols-outlined' style={{ cursor: 'pointer', marginRight: '45px' }} onClick={handleEdit} >🖊️</span>
-                <button onClick={() => setModalIsOpen(true)}>View Details</button>
-            </div>
-            
-            <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                <button style={{
-                    position: 'absolute',
-                    top: '10px',
-                    right: '10px',
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer',
-                    fontSize: '1.5rem',
-                    color: '#555',
-                    zIndex: '9999', // Ensure it appears above the modal content
-                }} onClick={() => setModalIsOpen(false)}>✖</button>
-                <h4 style={{color: '#e96914'}}>{new Date(appointment.pickupDate).toLocaleDateString('en-GB')}</h4>
+                <h4>{new Date(appointment.pickupDate).toLocaleDateString('en-GB')}</h4>
                 <p>
-                    <strong >Customer Name:</strong> {appointment.customerName}
+                    <strong>PickupLocation:</strong> <br/> {appointment.pickupLocation}
                 </p>
                 <p>
-                    <strong >Email:</strong> {appointment.email}
+                    <strong>Destination:</strong> <br/> {appointment.destination}
                 </p>
-                <p>
-                    <strong >Appointment created by:</strong> {appointment.username}
-                </p>
-                <p>
-                    <strong >Number of Passengers:</strong> {appointment.numberOfPassengers}
-                </p>
-                <p>
-                    <strong>Pickup Time:</strong> {appointment.pickupTime}
-                </p>
-                <p>
-                    <strong>Pickup Location:</strong> {appointment.pickupLocation}
-                </p>
-                <p>
-                    <strong>Destination:</strong> {appointment.destination}
-                </p>
-                <p>
-                    <strong>Is Return:</strong> {appointment.isReturn ? 'Yes' : 'No'}
-                </p>
-                {appointment.isReturn && (
-                    <>
-                        <p>
-                            <strong>Return Time:</strong> {appointment.returnTime}
-                        </p>
-                        <p>
-                            <strong>Return Location:</strong> {appointment.returnLocation}
-                        </p>
-                        <p>
-                            <strong>Return Destination:</strong> {appointment.returnDestination}
-                        </p>
-                    </>
-                )}
-                <p>
-                    <strong>Contact Number:</strong> {appointment.contactNumber}
-                </p>
-                <p>
-                    <strong>Price Quote:</strong> £ {appointment.priceQuote} 
-                </p>
-                {appointment.adInfo && (
-                    <p>
-                        <strong>Additional Information:</strong> {appointment.adInfo}
-                    </p>
-                )}
-
-            </Modal>
-            {deleteSuccess && (
-                <div className="success">
-                    Appointment deleted successfully
+                <div className="button-group">
+                    <span className='material-symbols-outlined' onClick={handleClick}>🗑️</span>
+                    <span className='material-symbols-outlined' style={{ cursor: 'pointer', marginRight: '45px' }} onClick={handleEdit} >🖊️</span>
+                    <button onClick={() => setModalIsOpen(true)}>View Details</button>
                 </div>
-            )}
-            {editMode ? (
-                console.log("modal"),
+
                 <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                <button style={{
-                    position: 'absolute',
-                    top: '10px',
-                    right: '10px',
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer',
-                    fontSize: '1.5rem',
-                    color: '#555',
-                    zIndex: '9999', // Ensure it appears above the modal content
-                }} onClick={() => setModalIsOpen(false)}>✖</button>
-                <div className="edit-form container">
-                    <form className="create" onSubmit={handleSubmit}>
-                        <h3>Edit Appointment</h3>
-                        <label>Customer Name:</label>
-                        <input
-                            type="text"
-                            value={customerName}
-                            onChange={(e) => setCustomerName(e.target.value)}
-                        />
-                        <label>Email:</label>
-                        <input
-                            type="email"
-                            placeholder="Email Address"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                        />
+                    <button style={{
+                        position: 'absolute',
+                        top: '10px',
+                        right: '10px',
+                        background: 'none',
+                        border: 'none',
+                        cursor: 'pointer',
+                        fontSize: '1.5rem',
+                        color: '#555',
+                        zIndex: '9999', // Ensure it appears above the modal content
+                    }} onClick={() => setModalIsOpen(false)}>✖</button>
+                    <h4 style={{ color: '#e96914' }}>{new Date(appointment.pickupDate).toLocaleDateString('en-GB')}</h4>
+                    <p>
+                        <strong >Customer Name:</strong> {appointment.customerName}
+                    </p>
+                    <p>
+                        <strong >Email:</strong> {appointment.email}
+                    </p>
+                    <p>
+                        <strong >Appointment created by:</strong> {appointment.username}
+                    </p>
+                    <p>
+                        <strong >Number of Passengers:</strong> {appointment.numberOfPassengers}
+                    </p>
+                    <p>
+                        <strong>Pickup Time:</strong> {appointment.pickupTime}
+                    </p>
+                    <p>
+                        <strong>Pickup Location:</strong> {appointment.pickupLocation}
+                    </p>
+                    <p>
+                        <strong>Destination:</strong> {appointment.destination}
+                    </p>
+                    <p>
+                        <strong>Is Return:</strong> {appointment.isReturn ? 'Yes' : 'No'}
+                    </p>
+                    {appointment.isReturn && (
+                        <>
+                            <p>
+                                <strong>Return Time:</strong> {appointment.returnTime}
+                            </p>
+                            <p>
+                                <strong>Return Location:</strong> {appointment.returnLocation}
+                            </p>
+                            <p>
+                                <strong>Return Destination:</strong> {appointment.returnDestination}
+                            </p>
+                        </>
+                    )}
+                    <p>
+                        <strong>Contact Number:</strong> {appointment.contactNumber}
+                    </p>
+                    <p>
+                        <strong>Price Quote:</strong> £ {appointment.priceQuote}
+                    </p>
+                    {appointment.adInfo && (
+                        <p>
+                            <strong>Additional Information:</strong> {appointment.adInfo}
+                        </p>
+                    )}
 
-                        <label>Number of Passengers:</label>
-                        <input
-                            type="text"
-                            value={numberOfPassengers}
-                            onChange={(e) => setNumberOfPassengers(e.target.value)}
-                        />
-                        <label>Is Return:</label>
-                        <input
-                            type="text"
-                            value={isReturn}
-                            onChange={(e) => setIsReturn(e.target.value)}
-                        />
-                        <label>Pickup Time:</label>
-                        <input
-                            type="text"
-                            value={pickupTime}
-                            onChange={(e) => setPickupTime(e.target.value)}
-                        />
-                        <label>Pickup Location:</label>
-                        <input
-                            type="text"
-                            value={pickupLocation}
-                            onChange={(e) => setPickupLocation(e.target.value)}
-                        />
-                        <label>Destination:</label>
-                        <input
-                            type="text"
-                            value={destination}
-                            onChange={(e) => setDestination(e.target.value)}
-                        />
-                        {isReturn && (
-                            <>
-                                <label>Return Time:</label>
-                                <input
-                                    type="text"
-                                    value={returnTime}
-                                    onChange={(e) => setReturnTime(e.target.value)}
-                                />
-                                <label>Return Location:</label>
-                                <input
-                                    type="text"
-                                    value={returnLocation}
-                                    onChange={(e) => setReturnLocation(e.target.value)}
-                                />
-                                <label>Return Destination:</label>
-                                <input
-                                    type="text"
-                                    value={returnDestination}
-                                    onChange={(e) => setReturnDestination(e.target.value)}
-                                />
-                            </>
-                        )}
-                        <label>Contact Number:</label>
-                        <input
-                            type="text"
-                            value={contactNumber}
-                            onChange={(e) => setContactNumber(e.target.value)}
-                        />
-                        <label>Additional Information:</label>
-                        <input
-                            type="text"
-                            value={adInfo}
-                            onChange={(e) => setAdInfo(e.target.value)}
-                            />
-                        <label>Price Quote:</label>
-                        <input
-                            type="text"
-                            value={priceQuote}
-                            onChange={(e) => setPriceQuote(e.target.value)}
-                            />
-
-                        <button style={{ marginRight: '10px' }}>Update</button>
-                        <button onClick={handleCancel}>Cancel</button>
-                    </form>
-                </div>
                 </Modal>
-            ) : null}
+                {deleteSuccess && (
+                    <div className="success">
+                        Appointment deleted successfully
+                    </div>
+                )}
+                {editMode ? (
+                    console.log("modal"),
+                    <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+                        <button style={{
+                            position: 'absolute',
+                            top: '10px',
+                            right: '10px',
+                            background: 'none',
+                            border: 'none',
+                            cursor: 'pointer',
+                            fontSize: '1.5rem',
+                            color: '#555',
+                            zIndex: '9999', // Ensure it appears above the modal content
+                        }} onClick={() => setModalIsOpen(false)}>✖</button>
+                        <div className="edit-form container">
+                            <form className="create" onSubmit={handleSubmit}>
+                                <h3>Edit Appointment</h3>
+                                
+                                <label>Pickup Date:</label>
+                                <input
+                                    type="date"
+                                    id="pickupDate"
+                                    value={pickupDate.substring(0, 10)}
+                                    onChange={(e) => setPickupDate(e.target.value)}
+                                />
+
+                                <label>Customer Name:</label>
+                                <input
+                                    type="text"
+                                    value={customerName}
+                                    onChange={(e) => setCustomerName(e.target.value)}
+                                />
+                                <label>Email:</label>
+                                <input
+                                    type="email"
+                                    placeholder="Email Address"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+
+                                <label>Number of Passengers:</label>
+                                <input
+                                    type="text"
+                                    value={numberOfPassengers}
+                                    onChange={(e) => setNumberOfPassengers(e.target.value)}
+                                />
+                                <label>Is Return:</label>
+                                <input
+                                    type="text"
+                                    value={isReturn}
+                                    onChange={(e) => setIsReturn(e.target.value)}
+                                />
+                                <label>Pickup Time:</label>
+                                <input
+                                    type="time"
+                                    value={pickupTime}
+                                    onChange={(e) => setPickupTime(e.target.value)}
+                                />
+                                <label>Pickup Location:</label>
+                                <input
+                                    type="text"
+                                    value={pickupLocation}
+                                    onChange={(e) => setPickupLocation(e.target.value)}
+                                />
+                                <label>Destination:</label>
+                                <input
+                                    type="text"
+                                    value={destination}
+                                    onChange={(e) => setDestination(e.target.value)}
+                                />
+                                {isReturn && (
+                                    <>
+                                        <label>Return Time:</label>
+                                        <input
+                                            type="time"
+                                            value={returnTime}
+                                            onChange={(e) => setReturnTime(e.target.value)}
+                                        />
+                                        <label>Return Location:</label>
+                                        <input
+                                            type="text"
+                                            value={returnLocation}
+                                            onChange={(e) => setReturnLocation(e.target.value)}
+                                        />
+                                        <label>Return Destination:</label>
+                                        <input
+                                            type="text"
+                                            value={returnDestination}
+                                            onChange={(e) => setReturnDestination(e.target.value)}
+                                        />
+                                    </>
+                                )}
+                                <label>Contact Number:</label>
+                                <input
+                                    type="text"
+                                    value={contactNumber}
+                                    onChange={(e) => setContactNumber(e.target.value)}
+                                />
+                                <label>Additional Information:</label>
+                                <input
+                                    type="text"
+                                    value={adInfo}
+                                    onChange={(e) => setAdInfo(e.target.value)}
+                                />
+                                <label>Price Quote:</label>
+                                <input
+                                    type="text"
+                                    value={priceQuote}
+                                    onChange={(e) => setPriceQuote(e.target.value)}
+                                />
+
+                                <button style={{ marginRight: '10px' }}>Update</button>
+                                <button onClick={handleCancel}>Cancel</button>
+                            </form>
+                        </div>
+                        
+                    </Modal>
+                ) : null}
+
+            </div>
             {error && <div className="error">{error}</div>}
-            {successMessage && <div className="success">{successMessage}</div>}
-        </div>
+                        {successMessage && <div className="success">{successMessage}</div>}
         </div>
     );
 };
